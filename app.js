8c6c2439f38333f87cfc4169ad0359cd15f340d7
Rupam Sutar <rupamsutar281998@gmail.com>
2023-05-16 23:07:33 +0530

    CRUD Operations

--- app.js
+++ app.js
@@ -37,10 +37,14 @@
 
 
 
-
+const fs = require("fs");
 const express = require("express");
-
 const app = express();
+
+//adding middleware
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 const port = 8000;
 
@@ -48,12 +52,57 @@ app.get("/", (req, res) => {
     res.status(200).json({message: "Get req accepted on the landing page.", status: 200});
 });
 
-app.post("/", (req,res) => {
-    res.json({
-        message: "post req accepted !",
-        status: 200
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: "Success",
+        result: tours.length, 
+        data: {
+            tours
+        }
+    })
+});
+
+app.post("/api/v1/tours", (req,res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = {
+        id: newId,
+        ...req.body
+    }
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour 
+            }
+        })
+    })
+})  
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    console.log(req.params);
+
+    const tour = tours.find(el => el.id === +req.params.id);
+    if (!tour) {
+        return res.status(404).json({
+            status: "Fail",
+            message: "Invalid ID"
+        })
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        }
     });
-})
+});
+
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
