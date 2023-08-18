5e30cad4eba8026bccac7b27287c844aeef8667c
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-08-18 12:55:58 +0530

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,8 +5,8 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   console.log(req.query);
 
-
-  // 1A) Filtering
+  try {
+     // 1A) Filtering
   const queryObj = {...req.query}
   const excludedFields = ["page", 'sort', 'limit', 'fields'];
   excludedFields.forEach((el) => delete queryObj[el]);
@@ -26,10 +26,27 @@ exports.getAllTours = async (req, res) => {
     query = query.sort(sortBy);
   }
 
+  // 3) Field Limiting
+  if(req.query.fields) {
+    const fields = req.query.fields.split(",").join(" ");
+    console.log(fields);
+    query = query.select(fields);
+  }
+
+  //4) Pagination
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 100;
+  const skip = (page - 1) * limit || 0;
+
+  if (req.query.page) {
+    const numTours = await Tour.countDocuments();
+    if (skip >= numTours) throw new Error("This page does not exist");
+  }
+
+  query = query.skip(skip).limit(limit);
   // const tours = await Tour.find().where("duration").equals(5).where("difficulty").equals("easy")
 
   const tours = await query;
-
 
   res.status(200).json({
     status: 'success',
@@ -38,6 +55,15 @@ exports.getAllTours = async (req, res) => {
       tours,
     },
   });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+
+
+ 
 };
 
 exports.getTour = async (req, res) => {
