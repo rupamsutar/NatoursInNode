c226389069b1dfe4e190fe53d8b1a1628a315f4f
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-09-26 17:42:13 +0530

    Managing Passwords

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,5 @@
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
-const catchAsync = require("../utils/catchAsync");
-const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -10,8 +8,8 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = catchAsync(async (req, res) => {
-
+exports.getAllTours = async (req, res) => {
+  try {
     // EXECUTE QUERY
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
@@ -28,16 +26,18 @@ exports.getAllTours = catchAsync(async (req, res) => {
         tours
       }
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-});
-
-exports.getTour = catchAsync(async (req, res, next) => {
-
+exports.getTour = async (req, res) => {
+  try {
     const tour = await Tour.findById(req.params.id);
-
-    if (!tour) {
-      return next(new AppError(`No tour found with the ID : ${req.params.id}`, 404));
-    }
+    // Tour.findOne({ _id: req.params.id })
 
     res.status(200).json({
       status: 'success',
@@ -45,52 +45,74 @@ exports.getTour = catchAsync(async (req, res, next) => {
         tour
       }
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-});
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
 
+    const newTour = await Tour.create(req.body);
 
-
-
-exports.createTour = catchAsync(async(req, res, next) => {
-  const newTour = await Tour.create(req.body);
     res.status(201).json({
       status: 'success',
       data: {
         tour: newTour
       }
     });
-})
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-exports.updateTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-})
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-exports.deleteTour = catchAsync(async (req, res, next) => {  
-   const tour = await Tour.findByIdAndDelete(req.params.id);
-
-    if (!tour) {
-      return next(new AppError(`No tour found with the ID : ${req.params.id}`, 404));
-    }
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
       status: 'success',
       data: null
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-});
-
-exports.getTourStats = catchAsync(async (req, res) => {
-
+exports.getTourStats = async (req, res) => {
+  try {
     const stats = await Tour.aggregate([
       {
         $match: { ratingsAverage: { $gte: 4.5 } }
@@ -120,11 +142,16 @@ exports.getTourStats = catchAsync(async (req, res) => {
         stats
       }
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-});
-
-exports.getMonthlyPlan = catchAsync(async (req, res) => {
-
+exports.getMonthlyPlan = async (req, res) => {
+  try {
     const year = req.params.year * 1; // 2021
 
     const plan = await Tour.aggregate([
@@ -168,5 +195,10 @@ exports.getMonthlyPlan = catchAsync(async (req, res) => {
         plan
       }
     });
-
-});
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
