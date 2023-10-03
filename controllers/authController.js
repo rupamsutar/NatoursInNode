d9e3e4eee790c7b9cfc54250feb978a718beaa6d
Rupam Sutar <107876344+rupamsutar@users.noreply.github.com>
2023-10-03 17:25:39 +0530

    Password Reset Functionality: Setting New Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModal');
@@ -49,6 +50,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3) If Everything OK send token to the client
   const token = signToken(user._id);
+  user.passwordChangedAt = undefined;
   res.status(200).json({
     status: 'success',
     token
@@ -107,7 +109,6 @@ exports.restrictTo = (...roles) => {
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get email based on posted email address.
-  console.log(req.body);
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(new AppError('No user found with this email address', 404));
@@ -146,4 +147,25 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async(req, res, next) => {
+    // 1) Get user based on the token
+    const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({passwordResetToken: hashedToken});
+
+    // 2) If token has not expired, and there is user, set the new password
+    if (!user) {
+        return next(new AppError('Token is invalid or has expired !', 400));
+    }
+    user.password = req.body.password;
+    user.confirmPassword = req.body.confirmPassword;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+    // 3) Update changed password at property for the user
+    // 4) Log the user in, send JWT.
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+});
