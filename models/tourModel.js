640141bf46014885038c7a46ce4c57fbc6318cf9
Rupam Sutar <rupamsutar281998@gmail.com>
2023-11-14 15:14:43 +0530

    Modelling Tour Guides: Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
+const User = require('../models/userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -77,7 +78,31 @@ const tourSchema = new mongoose.Schema(
     secretTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -92,6 +117,12 @@ tourSchema.virtual('durationWeeks').get(function() {
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromise = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromise);
   next();
 });
 
